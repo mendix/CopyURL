916d1217ff600502abfea20868b7a0d329b25c1c
Robert van 't Hof <robert.van.thof@mendix.com>
2016-01-21 17:04:32 +0100

    Now supports a prefix string

--- src/CopyURL/widget/CopyURL.js
+++ src/CopyURL/widget/CopyURL.js
@@ -34,7 +34,6 @@ define([
 
     // Declare widget's prototype.
     return declare("CopyURL.widget.CopyURL", [ _WidgetBase, _TemplatedMixin ], {
-        // _TemplatedMixin will create our dom node using this HTML template.
         templateString: widgetTemplate,
 
         // DOM elements
@@ -45,29 +44,22 @@ define([
         // Parameters configured in the Modeler.
         targetAttribute: "",
 
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-        _handles: null,
         _contextObj: null,
-        _alertDiv: null,
 
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function() {
             
             this._updateRendering();
             this._setupEvents();
         },
 
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function(obj, callback) {
 
             this._contextObj = obj;
-            this._resetSubscriptions();
             this._updateRendering();
 
             callback();
         },
 
-         // Attach events to HTML dom elements
         _setupEvents: function() {
             this.connect(this.copySelectNode, "click", function(e) {
                 var copyTextarea = this.copyInputNode;
@@ -85,97 +77,21 @@ define([
             
         },
 
-        // Rerender the interface.
         _updateRendering: function() {
  
             if (this._contextObj !== null) {
                 dojoStyle.set(this.domNode, "display", "block");
 
                 var toCopyValue = this._contextObj.get(this.targetAttribute);
+				
+				if (this.prefix) {
+					toCopyValue = this.prefix + toCopyValue;
+				}
 
                 this.copyInputNode.value = toCopyValue;
 
             } else {
                 dojoStyle.set(this.domNode, "display", "none");
-            }
-
-            // Important to clear all validations!
-            this._clearValidations();
-        },
-
-        // Handle validations.
-        _handleValidation: function(validations) {
-            this._clearValidations();
-
-            var validation = validations[0],
-                message = validation.getReasonByAttribute(this.backgroundColor);
-
-            if (this.readOnly) {
-                validation.removeAttribute(this.backgroundColor);
-            } else if (message) {
-                this._addValidation(message);
-                validation.removeAttribute(this.backgroundColor);
-            }
-        },
-
-        // Clear validations.
-        _clearValidations: function() {
-            dojoConstruct.destroy(this._alertDiv);
-            this._alertDiv = null;
-        },
-
-        // Show an error message.
-        _showError: function(message) {
-            if (this._alertDiv !== null) {
-                dojoHtml.set(this._alertDiv, message);
-                return true;
-            }
-            this._alertDiv = dojoConstruct.create("div", {
-                "class": "alert alert-danger",
-                "innerHTML": message
-            });
-            dojoConstruct.place(this.domNode, this._alertDiv);
-        },
-
-        // Add a validation.
-        _addValidation: function(message) {
-            this._showError(message);
-        },
-
-        // Reset subscriptions.
-        _resetSubscriptions: function() {
-            // Release handles on previous object, if any.
-            if (this._handles) {
-                dojoArray.forEach(this._handles, function (handle) {
-                    mx.data.unsubscribe(handle);
-                });
-                this._handles = [];
-            }
-
-            // When a mendix object exists create subscribtions. 
-            if (this._contextObj) {
-                var objectHandle = this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    callback: dojoLang.hitch(this, function(guid) {
-                        this._updateRendering();
-                    })
-                });
-
-                var attrHandle = this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    attr: this.backgroundColor,
-                    callback: dojoLang.hitch(this, function(guid, attr, attrValue) {
-                        this._updateRendering();
-                    })
-                });
-
-                var validationHandle = this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    val: true,
-                    callback: dojoLang.hitch(this, this._handleValidation)
-                });
-
-                this._handles = [ objectHandle, attrHandle, validationHandle ];
             }
         }
     });
